99ceabb05cccec3b1cc25ba429040fc7e586b93d
Francesco Grillea <f.grillea@reply.it>
2025-10-09 14:35:23 +0200

    (fix)[cm-proxy] instantiate custom ConfigurationManagerProxy

--- rv16_lib/architecture/base_service.py
+++ rv16_lib/architecture/base_service.py
@@ -3,7 +3,7 @@ from starlette import status
 from rv16_lib import logger
 from rv16_lib.exceptions import RV16Exception
 from rv16_lib.architecture.base_provider import BaseProvider
-from rv16_lib.configuration_manager import configuration_manager as cm
+from rv16_lib.configuration_manager import ConfigurationManagerProxy
 from rv16_lib.configuration_manager.entities import ServiceRegistrationRequest, ServicePairingRequest
 
 
@@ -13,7 +13,7 @@ class BaseService:
         self.service_name = None
         self.providers: dict[str, BaseProvider] = {}
 
-    def register_service(self, provider: str, configuration: dict):
+    def register_service(self, cm_proxy: ConfigurationManagerProxy, provider: str, configuration: dict):
         logger.info("Starting service registration...")
 
         request = ServiceRegistrationRequest(
@@ -21,7 +21,7 @@ class BaseService:
             service=self.service_name,
             configuration=configuration
         )
-        response = cm.register(request)
+        response = cm_proxy.register(request)
         logger.info(f"Service registration response: {response}")
         return response
 
