9ef1f967566a9384467732b2c62571c67d858a1e
Francesco Grillea <f.grillea@reply.it>
2025-10-23 12:12:03 +0200

    (fix)[srv-ocr]

--- rv16_lib/srv_ocr/entities.py
+++ rv16_lib/srv_ocr/entities.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from pydantic import BaseModel
+
 from rv16_lib.architecture.base_service_request import BaseServiceHttpRequest, BaseServicePayload
 
 
@@ -7,4 +9,11 @@ class OCRServiceConfig(BaseServiceHttpRequest, BaseServicePayload):
     ocr_path: Optional[str] = "ocr"
 
 class OCRServicePayload(BaseServicePayload):
+    ...
+
+class OCRServiceParams(BaseModel):
+    file_bytes: bytes
+    content_type: str
+
+class LocalOCRServiceParams(OCRServiceParams):
     ...
